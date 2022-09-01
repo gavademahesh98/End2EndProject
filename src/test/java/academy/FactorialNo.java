b78589f00a26623a8576abf995963259d32fe4eb
MAHESH <gavademahesh98@gmail.com>
2022-09-01 12:27:26 +0530

    commit palindrome by lead

--- src/test/java/academy/FactorialNo.java
+++ src/test/java/academy/FactorialNo.java
@@ -12,5 +12,15 @@ public class FactorialNo {
 		}
 		System.out.println(b);
 	}
+	@Test
+	public void Palindrome() {
+		String c="racecar";
+		String d="";
+		for(int i=c.length()-1;i>=0;i--) {
+			d=d+c.charAt(i);
+		}
+		if(d.equals(c))System.out.println("String C is Palindrome"); 
+		else System.out.println("String C is Not Palindrome");
+	}
 
 }
